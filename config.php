8972c51c14bf98a45dc588802e7c21e7256f51bd
Ferojbd <feroj8@gmail.com>
2024-11-30 22:30:34 +0600

    upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php 
+define('ROOT_DIR', __DIR__); 
+define('UPLOAD_DIR', ROOT_DIR . '/uploads'); 
+define('DATA_DIR', ROOT_DIR . '/data'); 
+require_once ROOT_DIR . '/vendor/autoload.php'; 
+require_once ROOT_DIR . '/.env'; 
